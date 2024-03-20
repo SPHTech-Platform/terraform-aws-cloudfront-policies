934d2c63957692aa717b46ea99985be8aab3ca76
niroz89 <niro@sph.com.sg>
2024-03-20 20:18:10 +0800

    added in wrong place

--- examples/response-headers/versions.tf
+++ examples/response-headers/versions.tf
@@ -1,8 +1,8 @@
 terraform {
   required_version = ">= 1.3"
 
-  # tflint-ignore: terraform_unused_required_providers
   required_providers {
+    # tflint-ignore: terraform_unused_required_providers
     aws = {
       source  = "hashicorp/aws"
       version = "~> 5.38"
